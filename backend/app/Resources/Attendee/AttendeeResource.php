8777cdf1f1eea82f2bb804dd7b59552e122d99a6
Godsaves Wandera <30301843+itswandera@users.noreply.github.com>
2025-10-09 17:54:41 +0300

    Update AttendeeResource.php

--- backend/app/Resources/Attendee/AttendeeResource.php
+++ backend/app/Resources/Attendee/AttendeeResource.php
@@ -25,6 +25,7 @@ class AttendeeResource extends JsonResource
             'product_price_id' => $this->getProductPriceId(),
             'event_id' => $this->getEventId(),
             'email' => $this->getEmail(),
+            'organization' => $this->getOrganization(),
             'status' => $this->getStatus(),
             'first_name' => $this->getFirstName(),
             'last_name' => $this->getLastName(),
